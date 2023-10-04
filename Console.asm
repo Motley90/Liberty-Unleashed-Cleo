877c1da0a183f3c56d60432752857ddfb4f1e245
Robert. R <motley679@gmail.com>
2023-10-03 21:28:48 -0400

    Update Console.asm
    Preparing for the module finder

--- Console.asm
+++ Console.asm
@@ -3,6 +3,18 @@ entry Main
 
 include 'win32a.inc'
 
+struct MODULEENTRY32
+        dwSize                  dd ?   
+        th32ModuleID            dd ? 
+        th32ProcessID           dd ? 
+        GlblcntUsage            dd ? 
+        ProccntUsage            dd ? 
+        modBaseAddr             dd ?   
+        modBaseSize             dd ? 
+        hModule                 dd ? 
+        szModule                rb 260 
+        szExePath               rb 1024
+ends
 section '.rdata' data readable
 
 ; Prints
@@ -12,9 +24,28 @@ gameFound db 'Game was found..', 10, 0
 
 gameHack db 'Bypassing anticheat security..', 10, 0
 
+section '.data' data readable writeable
 
-; Game Title 
+ProcessID dd ?
+ProcessHandle dd ?
+ 
+ModuleLocation dd ?
+
+TempProcEntry MODULEENTRY32
+TempSnapshot dd ?
+
+;========================================
+ 
+section '.rodata' data readable
 WindowTitle db 'Liberty Unleashed 0.1', 0
+ModuleHeader db 'LU.dll', 0
+ 
+PIDFormat db 'PID: %u', 0xD, 0xA, 0
+AddressFormat db '0x%lx', 0xD, 0xA, 0
+ModuleFormat db '%s @ 0x%x', 0xD, 0xA, 0
+
+;========================================
+
 
 section '.text' code executable
 Main:
@@ -23,14 +54,15 @@ Main:
         pop ecx
         push 0
 
-        jmp game
-game:
+
+        .game:
         invoke FindWindow, NULL, WindowTitle
         test eax,eax ; does it exist?
         jnz StartPatch ; Game found
-        jmp game ; retry
+        jmp .game ; retry
 
 StartPatch:
+
         push gameFound
         call [printf]
         pop ecx
@@ -50,6 +82,6 @@ sPause:
 section '.idata' data readable import
         library kernel32, 'kernel32.dll', \
         msvcrt, 'msvcrt.dll', user32,'USER32.DLL'
-        import kernel32, ExitProcess, 'ExitProcess'
-        import msvcrt, printf, 'printf'
-        import user32, FindWindow,'FindWindowA'
+        import kernel32, ExitProcess, 'ExitProcess', OpenProcess, 'OpenProcess', CreateToolhelp32Snapshot, 'CreateToolhelp32Snapshot', Module32First, 'Module32First', Module32Next, 'Module32Next',  CloseHandle, 'CloseHandle'
+        import msvcrt, printf, 'printf', stricmp, '_stricmp'
+        import user32, FindWindow,'FindWindowA', GetWindowThreadProcessId, 'GetWindowThreadProcessId'
